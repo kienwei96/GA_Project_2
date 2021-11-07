6ec1de9fdd110653d8cf01533ddc43c746bee832
kienwei96 <kien_wei@hotmail.com>
2021-11-07 13:01:10 +0800

    set CSS for Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Link} from 'react-router-dom'
 import Button from '@mui/material/Button';
-import "./SearchPanel.css";
+import "./Search.css";
 
 export default function Search() {
 
