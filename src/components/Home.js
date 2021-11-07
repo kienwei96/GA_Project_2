dcdae602846f9947bbe4f712b5de1791a94d0566
kienwei96 <kien_wei@hotmail.com>
2021-11-07 17:49:15 +0800

    update Home component to clear the local storage memories

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 
 
 export default function Home() {
+
+    localStorage.clear();
 
     return (
         <h1>This is home page.</h1>
