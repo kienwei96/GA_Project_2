09255b32f102618f4a5aa1973775570045408bc3
kienwei96 <kien_wei@hotmail.com>
2021-11-07 12:47:07 +0800

    Day-1 commit, set up route for main app and import hashrouter as router to index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import { Route, Switch } from "react-router";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import SearchHDB from "./components/SearchHDB";
+import NavBar from "./components/NavBar";
+import {
+  createTheme,
+  ThemeProvider,
+  responsiveFontSizes,
+} from "@mui/material/styles";
+
+let theme = createTheme({
+  typography: {
+    fontFamily: "Mulish, Arial",
+    body1: {
+      color: "#084c61",
+    },
+    button: {
+      textTransform: "none",
+      fontSize: "15px",
+      padding: "5px",
+      marginLeft: "5px",
+      fontWeight: 600,
+    },
+  },
+  palette: {
+    primary: {
+      main: "#084C61",
+      light: "#a8acbd",
+      dark: "#a8acbd",
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>
+    <NavBar/>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Switch>
+        <Route path="/search/hdbContractor">
+         <SearchHDB/>
+          </Route>
+          <Route path="/search">
+            <Search/>
+          </Route>
+        </Switch>
+      </main>
     </div>
+   
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App
