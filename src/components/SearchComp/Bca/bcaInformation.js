02bd314813f10de396c1b664e37eda2e61185a57
kienwei96 <kien_wei@hotmail.com>
2021-11-09 16:10:53 +0800

    create company information page to render the selected company details

---
+++ src/components/SearchComp/Bca/bcaInformation.js
@@ -0,0 +1,45 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { useParams, useHistory } from "react-router-dom";
+import "./bcaInformation.css";
+
+
+
+export default function Information(props) {
+
+    const params = useParams();
+    const history = useHistory();
+
+
+    const company = props.data?.filter( data => (data.id) === parseInt(params.id))
+    const company_id = (company[0].buildingID==="na")?' ':company[0].buildingID;
+    const company_street = (company[0].street==="na")?' ':company[0].street;
+    const company_unit = (company[0].unit==="na")?' ':company[0].unit;
+    const company_buildingName = (company[0].buildingName==="na")?' ':company[0].buildingName;
+    const company_postal = (company[0].postal==="na")?' ':"S"+company[0].postal;
+    
+
+
+    return (
+      <>
+      <h2>Company Information</h2>
+      <div className="body">
+        <div>
+          <div className="company_name">{`${company[0].name}`}</div>
+          <div>{`UEN No. : ${company[0].uen}`}</div>
+          <div>{`Address : ${company_id} ${company_street} ${company_unit} ${company_buildingName} ${company_postal}`}</div>
+          <div>{`Contact No. : ${company[0].contact}`}</div>
+          <div>{`Workhead : ${company[0].workhead}`}</div>
+          <div>{`Grade : ${company[0].grade}`}</div>
+          <div>{`License Expiry Date : ${company[0].expiry}`}</div>
+
+        </div>
+      </div>
+      <hr/>
+      <Button onClick={() => history.goBack()} sx={{ width: '80%', maxWidth: '150px', margin: '0 auto', height: '30px' }} variant="contained" >Back</Button>
+      </>
+
+    )
+
+
+}
