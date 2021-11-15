c19f5adca78823cb0ad91c36d94f556b93b1cd35
kienwei96 <kien_wei@hotmail.com>
2021-11-15 14:43:32 +0800

    set config.assetsPath to manage asset locally

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,3 +5,4 @@ import MapView from "@arcgis/core/views/MapView";
 import Graphic from "@arcgis/core/Graphic";
 import * as locator from "@arcgis/core/rest/locator";
 import esriConfig from "@arcgis/core/config.js";
+esriConfig.assetsPath = "./assets";
