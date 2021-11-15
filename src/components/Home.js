053f5b3bad6f3c26fe3cd3cf5b3108c7fc7fc17b
kienwei96 <kien_wei@hotmail.com>
2021-11-15 14:15:41 +0800

    add textDecoration style element to all Link to remove underline when mouse hover over

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import {Link} from 'react-router-dom';
 import Button from '@mui/material/Button';
-import Paper from '@mui/material/Paper';
-import Stack from '@mui/material/Stack';
-import { styled } from '@mui/material/styles';
 import renovation from '../image/renovation.png';
 import window from '../image/windows.png';
 import plumbing from '../image/plumbing.png';
@@ -12,65 +9,55 @@ import repair from '../image/repair.png';
 import crack from '../image/crack.png';
 import "./Home.css";
 
-const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: 'white',
-    backgroundColor: 'grey',
-  }));
+
 
 
 
 export default function Home() {
 
     localStorage.clear();
-    const [category, setCategory] = useState();
 
-    const handleClick =() => {
-     
-  }
+    const categories = {
+      renovation: 'renovation',
+      windows: 'RW01',
+      plumbing: 'ME12',
+      concreterepair: 'CR05',
+      waterproof: 'CR13',
+      aircon: 'ME01',
+    }
+
 
     return (
         <>
         <div className="outer">
         <div className="Home">
-        <h1>Choose the type of contractor you are looking for.</h1>
         <div className="list">
           <div className="inner">
-          <Link style={{ textDecoration: 'none' }} to='/renovation'><Button sx={{width: 250}}variant="contained" ><img style={{marginRight: '10px'}}src={renovation} alt="renovation" width="30" height="30"></img>Renovation</Button>
+          <Link style={{ textDecoration: 'none' }} to={"/" + categories.renovation}><Button sx={{width: 250}}variant="contained" ><img style={{marginRight: '10px'}}src={renovation} alt="renovation" width="30" height="30"></img>Renovation</Button>
         </Link>
           </div>
           <div className="inner">
-          <Link style={{ textDecoration: 'none' }} to='/windows'><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={window} alt="window" width="30" height="30"></img>Window</Button>
+          <Link style={{ textDecoration: 'none' }} to={"/" + categories.windows}><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={window} alt="window" width="30" height="30"></img>Window</Button>
         </Link>
           </div>
           <div className="inner">
-          <Link style={{ textDecoration: 'none' }} to='/plumbing'><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={plumbing} alt="plumbing" width="30" height="30"></img>Plumbing and Sanitary</Button>
+          <Link style={{ textDecoration: 'none' }} to={"/" + categories.plumbing}><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={plumbing} alt="plumbing" width="30" height="30"></img>Plumbing and Sanitary</Button>
         </Link>
           </div>
           <div className="inner">
-          <Link style={{ textDecoration: 'none' }} to='/concreterepair'><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={crack} alt="crack" width="30" height="30"></img>Concrete Cracks Repair</Button>
+          <Link style={{ textDecoration: 'none' }} to={"/" + categories.concreterepair}><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={crack} alt="crack" width="30" height="30"></img>Concrete Cracks Repair</Button>
         </Link>
           </div>
           <div className="inner">
-          <Link style={{ textDecoration: 'none' }} to='/waterproof'><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={waterproof} alt="waterproof" width="30" height="30"></img>Waterproofing</Button>
+          <Link style={{ textDecoration: 'none' }} to={"/" + categories.waterproof}><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={waterproof} alt="waterproof" width="30" height="30"></img>Waterproofing</Button>
         </Link>
           </div>
           <div className="inner">
-          <Link style={{ textDecoration: 'none' }} to='/mechanical'><Button variant="contained" ><img style={{marginRight: '10px'}}src={repair} alt="repair" width="30" height="30"></img>Mechanical and Electrical</Button>
+          <Link style={{ textDecoration: 'none' }} to={"/" + categories.aircon}><Button sx={{width: 250}} variant="contained" ><img style={{marginRight: '10px'}}src={repair} alt="repair" width="30" height="30"></img>Air-conditioning (M&E)</Button>
         </Link>
           </div>
-        {/* <Stack spacing={3}>
-        <Item><img className="image" src={renovation} alt="renovation" width="30" height="30"></img>Renovation</Item>
-        <Item>Window</Item>
-        <Item>Plumbing and Sanitary</Item>
-        <Item>Concrete Repair</Item>
-        <Item>Waterproofing</Item>
-        <Item>Mechanical and Electrical</Item>
-      </Stack> */}
-    </div>
-    </div>
+        </div>
+      </div>
     </div>
     </>
     )
