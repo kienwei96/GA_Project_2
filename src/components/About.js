4b0e74192eac4bf928643fb150b5a9ca884d6cf1
kienwei96 <kien_wei@hotmail.com>
2021-11-15 21:35:31 +0800

    update About page

--- src/components/About.js
+++ src/components/About.js
@@ -1,25 +1,49 @@
-import React from 'react';
+import React from "react";
+import { GitHub, LinkedIn } from "@material-ui/icons";
+import Typography from "@mui/material/Typography";
 
+const About = () => {
+  return (
+    <Typography component="span">
+      <div
+        style={{
+          display: "flex",
+          flexFlow: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "80vh",
+          width: "100vw",
+        }}
+      >
+        <div>
+          This site was created using ReactJS
+          <br />
+          <br />
+          Maps: ArcGIS API for JavaScript
+          <br />
+          Tables: material-table
+          <br />
+          Styling: Material-UI
+          <br />
+          <br />
+          List of contractor data from data.gov.sg API<br/>
 
+        </div>
+        <div>
+          <a href="https://github.com/kienwei96/GA_Project_2" target="blank">
+            <GitHub
+              style={{ padding: "30px", fontSize: "45px", color: "#5fa8ab" }}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/kienwei96" target="blank">
+            <LinkedIn
+              style={{ margin: "30px", fontSize: "45px", color: "#5fa8ab" }}
+            />
+          </a>
+        </div>
+      </div>
+    </Typography>
+  );
+};
 
-export default function About() {
-
-    localStorage.clear();
-    
-    return (
-        <h1>This is About page.</h1>
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
+export default About;
