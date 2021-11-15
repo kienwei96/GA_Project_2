972388ebeb46d5945dbb0bf48ef599534904cc5c
kienwei96 <kien_wei@hotmail.com>
2021-11-15 14:42:49 +0800

    import from arcgis dependencies

---
+++ src/components/Map.js
@@ -0,0 +1,7 @@
+import './App.css';
+import React, { useRef, useEffect } from "react";
+import WebMap from "@arcgis/core/WebMap";
+import MapView from "@arcgis/core/views/MapView";
+import Graphic from "@arcgis/core/Graphic";
+import * as locator from "@arcgis/core/rest/locator";
+import esriConfig from "@arcgis/core/config.js";
