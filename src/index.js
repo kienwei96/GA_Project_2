aed4a6b6da424d8539e9b4bd806cdd96a9ab5cb7
kienwei96 <kien_wei@hotmail.com>
2021-11-16 22:04:06 +0800

    adjust css

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
 import {HashRouter as Router} from "react-router-dom"
+import './index.css';
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
